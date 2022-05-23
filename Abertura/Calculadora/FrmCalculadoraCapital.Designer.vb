ce11cccb9fde448e110706a50acf797c42ed428c
Rogerio Stranieri <rogeriostran@gmail.com>
2022-05-23 17:48:25 -0300

    23/05/2022 teste iniciado calculadora de capital social

--- Abertura/Calculadora/FrmCalculadoraCapital.Designer.vb
+++ Abertura/Calculadora/FrmCalculadoraCapital.Designer.vb
@@ -22,32 +22,36 @@ Partial Class FrmCalculadoraCapital
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBoxCapitalSocial = New System.Windows.Forms.TextBox()
+        Me.ButtonPorcentagem = New System.Windows.Forms.Button()
         Me.LblCapTotal = New System.Windows.Forms.Label()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.ColumnSocio = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.ColumnPorcentagem = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.ColumnAntigo = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.ColumnNovo = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.GanhaEPerca = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ButtonDistribuir = New System.Windows.Forms.Button()
+        Me.ButtonLimpar = New System.Windows.Forms.Button()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'TextBox1
+        'TextBoxCapitalSocial
         '
-        Me.TextBox1.Location = New System.Drawing.Point(128, 36)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(140, 20)
-        Me.TextBox1.TabIndex = 0
+        Me.TextBoxCapitalSocial.Location = New System.Drawing.Point(128, 36)
+        Me.TextBoxCapitalSocial.Name = "TextBoxCapitalSocial"
+        Me.TextBoxCapitalSocial.Size = New System.Drawing.Size(140, 20)
+        Me.TextBoxCapitalSocial.TabIndex = 0
+        Me.TextBoxCapitalSocial.Text = "20000"
         '
-        'Button1
+        'ButtonPorcentagem
         '
-        Me.Button1.Location = New System.Drawing.Point(589, 245)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(89, 20)
-        Me.Button1.TabIndex = 2
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.ButtonPorcentagem.Location = New System.Drawing.Point(592, 81)
+        Me.ButtonPorcentagem.Name = "ButtonPorcentagem"
+        Me.ButtonPorcentagem.Size = New System.Drawing.Size(89, 20)
+        Me.ButtonPorcentagem.TabIndex = 2
+        Me.ButtonPorcentagem.Text = "% Porcentgem"
+        Me.ButtonPorcentagem.UseVisualStyleBackColor = True
         '
         'LblCapTotal
         '
@@ -62,10 +66,10 @@ Partial Class FrmCalculadoraCapital
         '
         Me.DataGridView1.AllowUserToOrderColumns = True
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.ColumnSocio, Me.ColumnPorcentagem, Me.ColumnAntigo, Me.ColumnNovo})
-        Me.DataGridView1.Location = New System.Drawing.Point(59, 234)
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.ColumnSocio, Me.ColumnPorcentagem, Me.ColumnAntigo, Me.ColumnNovo, Me.GanhaEPerca})
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 81)
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(486, 150)
+        Me.DataGridView1.Size = New System.Drawing.Size(561, 150)
         Me.DataGridView1.TabIndex = 5
         '
         'ColumnSocio
@@ -88,16 +92,42 @@ Partial Class FrmCalculadoraCapital
         Me.ColumnNovo.HeaderText = "Capital Novo"
         Me.ColumnNovo.Name = "ColumnNovo"
         '
+        'GanhaEPerca
+        '
+        Me.GanhaEPerca.HeaderText = "Ganhou/Perdeu"
+        Me.GanhaEPerca.Name = "GanhaEPerca"
+        '
+        'ButtonDistribuir
+        '
+        Me.ButtonDistribuir.Location = New System.Drawing.Point(592, 107)
+        Me.ButtonDistribuir.Name = "ButtonDistribuir"
+        Me.ButtonDistribuir.Size = New System.Drawing.Size(89, 20)
+        Me.ButtonDistribuir.TabIndex = 6
+        Me.ButtonDistribuir.Text = "Distribuição"
+        Me.ButtonDistribuir.UseVisualStyleBackColor = True
+        '
+        'ButtonLimpar
+        '
+        Me.ButtonLimpar.Location = New System.Drawing.Point(484, 237)
+        Me.ButtonLimpar.Name = "ButtonLimpar"
+        Me.ButtonLimpar.Size = New System.Drawing.Size(89, 20)
+        Me.ButtonLimpar.TabIndex = 7
+        Me.ButtonLimpar.Text = "Limpar"
+        Me.ButtonLimpar.UseVisualStyleBackColor = True
+        '
         'FrmCalculadoraCapital
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(702, 269)
+        Me.Controls.Add(Me.ButtonLimpar)
+        Me.Controls.Add(Me.ButtonDistribuir)
         Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.LblCapTotal)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ButtonPorcentagem)
+        Me.Controls.Add(Me.TextBoxCapitalSocial)
         Me.Name = "FrmCalculadoraCapital"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "FrmCalculadoraCapital"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
@@ -105,12 +135,15 @@ Partial Class FrmCalculadoraCapital
 
     End Sub
 
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBoxCapitalSocial As TextBox
+    Friend WithEvents ButtonPorcentagem As Button
     Friend WithEvents LblCapTotal As Label
     Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents ButtonDistribuir As Button
     Friend WithEvents ColumnSocio As DataGridViewTextBoxColumn
     Friend WithEvents ColumnPorcentagem As DataGridViewTextBoxColumn
     Friend WithEvents ColumnAntigo As DataGridViewTextBoxColumn
     Friend WithEvents ColumnNovo As DataGridViewTextBoxColumn
+    Friend WithEvents GanhaEPerca As DataGridViewTextBoxColumn
+    Friend WithEvents ButtonLimpar As Button
 End Class
--- Abertura/Calculadora/FrmCalculadoraCapital.vb
+++ Abertura/Calculadora/FrmCalculadoraCapital.vb
@@ -1,23 +1,190 @@
 ﻿Public Class FrmCalculadoraCapital
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        'pegal total capital social e calcula porcentagem do  DataGridView1 na coluna "Porcentagem"
-        'verificar se o valor do capital social é maior que zero
-        'verificar linha preenchida no campo porceentagem
 
-        Dim valor As Double
-        Dim porcentagem As Double
-        Dim total As Double
-        total = CDbl(TextBox1.Text)
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ButtonPorcentagem.Click
+        Try
+            ' nao ultrapassar 100% da cell 1 do capital social do TextBoxCapitalSocial
+            If TextBoxCapitalSocial.Text = "" Then
+                MsgBox("Digite o valor do capital social")
+                TextBoxCapitalSocial.Focus()
+                Exit Sub
+            End If
 
-        If DataGridView1.Rows.Count > 0 Then
+            'cada linha tem seu calculo de porcentagem cell 1,sob o valor da TextBoxCapitalSocial 
+            'conveter Double
+            Dim valor As Double = CDbl(TextBoxCapitalSocial.Text)
+
+
             For Each row As DataGridViewRow In DataGridView1.Rows
-                If row.Cells(0).Value <> "" Then
-                    valor = total
-                    porcentagem = row.Cells(1).Value
-                    row.Cells(2).Value = valor * (porcentagem / 100)
+                If row.Cells(1).Value = "" Then
+                    row.Cells(1).Value = 0
+                End If
+                'calculo da porcentagem
+                'Cells(3) porcentagem
+                row.Cells(3).Value = (row.Cells(1).Value * valor) / 100
+                'coluna 3 converte em Double
+                row.Cells(3).Value = CDbl(row.Cells(3).Value)
+                'coluna 3 converte em String
+                row.Cells(3).Value = FormatCurrency(row.Cells(3).Value)
+
+            Next
+
+
+
+
+
+            'depois totalizar o valores das em uma nova linha, e nao repetir o calculo
+            'se tiver TOTAL limpas e fazer calculo novamente
+            If DataGridView1.Rows.Count > 0 Then
+                For Each row As DataGridViewRow In DataGridView1.Rows
+                    If row.Cells(0).Value = "TOTAL" Then
+                        DataGridView1.Rows.Remove(row)
+                    End If
+                Next
+            End If
+
+
+            'double
+            Dim total As Double = 0
+            For i = 0 To DataGridView1.Rows.Count - 2
+
+                DataGridView1.Rows(DataGridView1.Rows.Count - 1).Cells(0).Value = "Total"
+                'em negrito
+                DataGridView1.Rows(DataGridView1.Rows.Count - 1).Cells(0).Style.Font = New Font("Arial", 10, FontStyle.Bold)
+                DataGridView1.Rows(DataGridView1.Rows.Count - 1).Cells(1).Value = ""
+                DataGridView1.Rows(DataGridView1.Rows.Count - 1).Cells(2).Value = ""
+                DataGridView1.Rows(DataGridView1.Rows.Count - 1).Cells(3).Value = ""
+
+
+
+
+                total = total + DataGridView1.Rows(i).Cells(3).Value
+
+                'Valor da porcentagem ultrapassou o valor do capital social
+                If total > valor Then
+                    MsgBox("Valor da porcentagem ultrapassou o valor do capital social")
+
+                    Exit Sub
+                End If
+
+                'DataGridView1 Cells(1), nao passar de 100, conveter em doble
+                If DataGridView1.Rows(i).Cells(1).Value > 100 Then
+                    MsgBox("Valor da porcentagem ultrapassou o valor do capital social")
+                    Exit Sub
                 End If
             Next
-        End If
 
+
+
+
+
+
+            'no final converte Double em reais a cell 3
+            DataGridView1.Rows(DataGridView1.Rows.Count - 1).Cells(3).Value = FormatCurrency(total)
+
+            For i = 0 To DataGridView1.Rows.Count - 1
+                'DataGridView1.Rows(i).Cells(3).Value = FormatCurrency(DataGridView1.Rows(i).Cells(3).Value)
+                DataGridView1.Rows(DataGridView1.Rows.Count - 1).Cells(3).Value = FormatCurrency(total)
+
+            Next
+
+
+            'verificar se tem valor preenchido na cell2
+            For Each row As DataGridViewRow In DataGridView1.Rows
+                If row.Cells(2).Value <> "" Then
+                    PercaGanho()
+                End If
+            Next
+
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub PercaGanho()
+        Try
+
+
+            'coluna 2 e 3 nao podem ser vazio
+            If DataGridView1.Rows(0).Cells(2).Value = "" Then
+                MsgBox("Coluna 2 não pode ser vazia")
+                Exit Sub
+            End If
+
+
+
+            If DataGridView1.Rows(0).Cells(3).Value = "" Then
+                MsgBox("Coluna 3 não pode ser vazia")
+                Exit Sub
+            End If
+
+
+            For Each row As DataGridViewRow In DataGridView1.Rows
+                'Coluna2 converter string e Double
+                Dim coluna2 As Double = CDbl(DataGridView1.Rows(0).Cells(2).Value)
+                Dim coluna3 As Double = CDbl(DataGridView1.Rows(0).Cells(3).Value)
+
+
+                'coluna 4 = coluna 3 - coluna 2 de
+                'todas as Rows
+
+
+
+                row.Cells(4).Value = coluna3 - coluna2
+                '  DataGridView1.Rows(0).Cells(4).Value = coluna3 - coluna2
+
+
+
+
+
+                'coluna4 converte em Double
+                DataGridView1.Rows(0).Cells(4).Value = CDbl(DataGridView1.Rows(0).Cells(4).Value)
+                'coluna4 converte em String
+                DataGridView1.Rows(0).Cells(4).Value = FormatCurrency(DataGridView1.Rows(0).Cells(4).Value)
+                'letra negrito
+                DataGridView1.Rows(0).Cells(4).Style.Font = New Font("Arial", 10, FontStyle.Bold)
+                'coluna2 converte em Double
+                DataGridView1.Rows(0).Cells(2).Value = CDbl(DataGridView1.Rows(0).Cells(2).Value)
+                'coluna2 converte em String
+                DataGridView1.Rows(0).Cells(2).Value = FormatCurrency(DataGridView1.Rows(0).Cells(2).Value)
+
+
+
+
+
+                'pintar em vermelho o valor negativo
+                If row.Cells(4).Value < 0 Then
+                        row.Cells(4).Style.BackColor = Color.Red
+                        'letra branca
+                        row.Cells(4).Style.ForeColor = Color.White
+                        'negrito
+                        row.Cells(4).Style.Font = New Font(DataGridView1.Font, FontStyle.Bold)
+                    End If
+                    'pintar em verde valor positivo
+                    If row.Cells(4).Value > 0 Then
+                        row.Cells(4).Style.BackColor = Color.Green
+                        'letra branca
+                        row.Cells(4).Style.ForeColor = Color.White
+                        'negrito
+                        row.Cells(4).Style.Font = New Font(DataGridView1.Font, FontStyle.Bold)
+                    End If
+                Next
+
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+    Private Sub ButtonDistribuir_Click(sender As Object, e As EventArgs) Handles ButtonDistribuir.Click
+
+        PercaGanho()
+
+    End Sub
+
+    Private Sub ButtonLimpar_Click(sender As Object, e As EventArgs) Handles ButtonLimpar.Click
+        'limpaar
+        DataGridView1.Rows.Clear()
+        DataGridView1.Refresh()
     End Sub
 End Class
