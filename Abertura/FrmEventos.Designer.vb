a3edd41a2304891e5db28ecf461cffb6db529316
Rogerio Stranieri <rogeriostran@gmail.com>
2022-10-04 17:09:09 -0300

    04/10/2022 as 17:08 atualização do form Eventos modificando sua lista de eventos, aparencia e codigos

--- Abertura/FrmEventos.Designer.vb
+++ Abertura/FrmEventos.Designer.vb
@@ -26,37 +26,41 @@ Partial Class FrmEventos
         Me.CheckedListBoxEventosJunta = New System.Windows.Forms.CheckedListBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.BtnEventos = New System.Windows.Forms.Button()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.BtnFechar = New System.Windows.Forms.PictureBox()
         EventosLabel = New System.Windows.Forms.Label()
+        CType(Me.BtnFechar, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'EventosLabel
         '
         EventosLabel.AutoSize = True
+        EventosLabel.BackColor = System.Drawing.Color.Transparent
         EventosLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        EventosLabel.Location = New System.Drawing.Point(12, 106)
+        EventosLabel.Location = New System.Drawing.Point(12, 135)
         EventosLabel.Name = "EventosLabel"
-        EventosLabel.Size = New System.Drawing.Size(261, 13)
+        EventosLabel.Size = New System.Drawing.Size(69, 13)
         EventosLabel.TabIndex = 2
-        EventosLabel.Text = "*** LISTA DE EVENTOS DE ALTERAÇÃO ***"
+        EventosLabel.Text = "EVENTOS:"
         '
         'CheckedListBoxEventosJunta
         '
         Me.CheckedListBoxEventosJunta.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.CheckedListBoxEventosJunta.FormattingEnabled = True
-        Me.CheckedListBoxEventosJunta.Items.AddRange(New Object() {"=======FILIAL=======", "Abertura de Filial", "Alteração de Filial", "Baixa de Filial", "", "=======JUNTA COMERCIAL=======", "Alteração da forma de atuação", "Alteração da natureza jurídica", "Alteração de atividades econômicas (principal e secundárias)", "Alteração de capital social e/ou Quadro Societário", "Alteração de Cláusulas Particulares", "Alteração de dados do Empresário", "Alteração de endereço entre estados", "Alteração de endereço entre municípios no mesmo estado", "Alteração de endereço no mesmo município", "Alteração de nome empresarial (firma ou denominação)", "Alteração de Proteção de Nome Empresarial", "Alteração do tipo de unidade", "Alteração da pessoa física responsável perante o CNPJ", "Cancelamento de Proteção de Nome Empresarial", "Consolidação", "Emancipação", "Enquadramento / Reenquadramento / Desenquadramento de Porte de Empresa", "Espólio", "Interrupção temporária de atividades", "Procuração", "Proteção de Nome Empresarial", "Reinício das atividades interrompidas temporariamente", "Rerratificação", "Restabelecimento de filial", "Restabelecimento de matriz (Reativação)", "Revogação de Procuração", "", "=======RECEITA FEDERAL=======", "Abertura apenas na Receita Federal", "Alteração apenas na Receita Federal", "Baixa apenas na Receita Federal", "Alteração da pessoa fisica responsável perante o CNPJ", "", "=======RECEITA ESTADUAL=======", "Abertura apenas na Receita Estadual", "Alteração apenas na Receita Estadual", "Baixa apenas na Receita Estadual"})
-        Me.CheckedListBoxEventosJunta.Location = New System.Drawing.Point(15, 122)
+        Me.CheckedListBoxEventosJunta.Items.AddRange(New Object() {"=======JUNTA COMERCIAL=======", "Abertura Matriz", "Baixa Matriz", "", "Abertura de Filial", "Alteração de Filial", "Baixa de Filial", "", "Alteração da forma de atuação", "Alteração da natureza jurídica", "Alteração de atividades econômicas (principal e secundárias)", "Alteração de capital social e/ou Quadro Societário", "Alteração de Cláusulas Particulares", "Alteração de dados do Empresário", "Alteração de endereço entre estados", "Alteração de endereço entre municípios no mesmo estado", "Alteração de endereço no mesmo município", "Alteração de nome empresarial (firma ou denominação)", "Alteração de Proteção de Nome Empresarial", "Alteração do tipo de unidade", "Alteração da pessoa física responsável perante o CNPJ", "Cancelamento de Proteção de Nome Empresarial", "Consolidação", "Emancipação", "Enquadramento / Reenquadramento / Desenquadramento de Porte de Empresa", "Espólio", "Interrupção temporária de atividades", "Procuração", "Proteção de Nome Empresarial", "Reinício das atividades interrompidas temporariamente", "Rerratificação", "Restabelecimento de filial", "Restabelecimento de matriz (Reativação)", "Revogação de Procuração", "", "=======RECEITA FEDERAL=======", "Abertura apenas na Receita Federal", "Alteração apenas na Receita Federal", "Baixa apenas na Receita Federal", "Alteração da pessoa fisica responsável perante o CNPJ", "", "=======RECEITA ESTADUAL=======", "Abertura apenas na Receita Estadual", "Alteração apenas na Receita Estadual", "Baixa apenas na Receita Estadual"})
+        Me.CheckedListBoxEventosJunta.Location = New System.Drawing.Point(15, 156)
         Me.CheckedListBoxEventosJunta.Name = "CheckedListBoxEventosJunta"
-        Me.CheckedListBoxEventosJunta.Size = New System.Drawing.Size(532, 327)
+        Me.CheckedListBoxEventosJunta.Size = New System.Drawing.Size(532, 293)
         Me.CheckedListBoxEventosJunta.TabIndex = 3
         '
         'Label1
         '
         Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(180, 9)
+        Me.Label1.Location = New System.Drawing.Point(11, 18)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(205, 24)
         Me.Label1.TabIndex = 4
@@ -65,6 +69,7 @@ Partial Class FrmEventos
         'BtnEventos
         '
         Me.BtnEventos.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnEventos.ForeColor = System.Drawing.Color.Green
         Me.BtnEventos.Location = New System.Drawing.Point(454, 455)
         Me.BtnEventos.Name = "BtnEventos"
         Me.BtnEventos.Size = New System.Drawing.Size(93, 30)
@@ -72,19 +77,10 @@ Partial Class FrmEventos
         Me.BtnEventos.Text = "Adicionar"
         Me.BtnEventos.UseVisualStyleBackColor = True
         '
-        'Button1
-        '
-        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(488, 3)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(66, 30)
-        Me.Button1.TabIndex = 6
-        Me.Button1.Text = "Fechar"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
         'Button2
         '
         Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.Red
         Me.Button2.Location = New System.Drawing.Point(382, 455)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(66, 30)
@@ -94,19 +90,46 @@ Partial Class FrmEventos
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(15, 73)
+        Me.TextBox1.Location = New System.Drawing.Point(15, 96)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(532, 20)
         Me.TextBox1.TabIndex = 8
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 77)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 16)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Localizar:"
+        '
+        'BtnFechar
+        '
+        Me.BtnFechar.BackColor = System.Drawing.Color.Transparent
+        Me.BtnFechar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.BtnFechar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BtnFechar.Image = Global.PrinceSistemas.My.Resources.Resources.fechar2
+        Me.BtnFechar.Location = New System.Drawing.Point(413, -2)
+        Me.BtnFechar.Name = "BtnFechar"
+        Me.BtnFechar.Size = New System.Drawing.Size(134, 60)
+        Me.BtnFechar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.BtnFechar.TabIndex = 15
+        Me.BtnFechar.TabStop = False
         '
         'FrmEventos
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.PrinceSistemas.My.Resources.Resources.fundo_azul
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.ClientSize = New System.Drawing.Size(566, 492)
+        Me.Controls.Add(Me.BtnFechar)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.BtnEventos)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.CheckedListBoxEventosJunta)
@@ -118,6 +141,7 @@ Partial Class FrmEventos
         Me.ShowIcon = False
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Lista de Eventos"
+        CType(Me.BtnFechar, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -126,7 +150,8 @@ Partial Class FrmEventos
     Friend WithEvents CheckedListBoxEventosJunta As CheckedListBox
     Friend WithEvents Label1 As Label
     Friend WithEvents BtnEventos As Button
-    Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents BtnFechar As PictureBox
 End Class
--- Abertura/FrmEventos.vb
+++ Abertura/FrmEventos.vb
@@ -16,15 +16,11 @@
 
 
             Next
-            Me.Close()
+            'Me.Close()
 
         End If
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Me.Close()
-
-    End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         If CheckedListBoxEventosJunta.CheckedItems.Count <> 0 Then
@@ -35,7 +31,7 @@
                 'limpar
 
                 FrmLegalizacao.MotivoRichTextBox.Clear()
-                Me.Close()
+                ' Me.Close()
             Next
 
 
@@ -43,35 +39,37 @@
     End Sub
 
 
+    'ao digitar no  TextBox1 vai filtrar dentro da CheckedListBoxEventosJunta
+    'nao diferenciando minuscula ou maiuscula
     Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
-
-        ' Dim items = From it In CheckedListBoxEventosJunta.Items.Cast(Of Object)()
-        '             Where it.ToString().IndexOf(TextBox1.Text, StringComparison.CurrentCultureIgnoreCase) >= 0
-
-
-        ' Dim matchingItemList As List(Of Object) = items.ToList()
-        'CheckedListBoxEventosJunta.BeginUpdate()
-        'CheckedListBoxEventosJunta.Items.Clear()
-
-
-        'For Each item In matchingItemList
-
-        '   CheckedListBoxEventosJunta.Items.Add(item)
-
-        ' Next
-
-        ' CheckedListBoxEventosJunta.EndUpdate()
-
-
-        Dim i As Integer = CheckedListBoxEventosJunta.FindString(TextBox1.Text)
-
-
-
-        CheckedListBoxEventosJunta.SelectedIndex = i
-
-
-
+        Dim i As Integer
+        For i = 0 To CheckedListBoxEventosJunta.Items.Count - 1
+            If CheckedListBoxEventosJunta.Items(i).ToString().ToLower().Contains(TextBox1.Text.ToLower()) Then
+                CheckedListBoxEventosJunta.SetItemChecked(i, True)
+            Else
+                CheckedListBoxEventosJunta.SetItemChecked(i, False)
+            End If
+        Next
     End Sub
 
 
+    Private Sub CheckedListBoxEventosJunta_Click(sender As Object, e As EventArgs) Handles CheckedListBoxEventosJunta.Click
+        'selecionar apenas onde for clicado e desmarcar o resto do ListBox
+        Dim i As Integer
+        Dim item As String
+        Dim texto As String
+        texto = CheckedListBoxEventosJunta.SelectedItem.ToString
+        For i = 0 To CheckedListBoxEventosJunta.Items.Count - 1
+            item = CheckedListBoxEventosJunta.Items(i).ToString
+            If item.Contains(texto) Then
+                CheckedListBoxEventosJunta.SetItemChecked(i, True)
+            Else
+                CheckedListBoxEventosJunta.SetItemChecked(i, False)
+            End If
+        Next
+    End Sub
+
+    Private Sub BtnFechar_Click(sender As Object, e As EventArgs) Handles BtnFechar.Click
+        Me.Close()
+    End Sub
 End Class
